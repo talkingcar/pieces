9f20efc6b27cdbc8cd3a0286d5f5a128404ae9dc
Sam Brown <talkingcar@mac.com>
2020-07-18 16:02:55 -0400

    turned 'prettier' on

--- ball_that_bounces/script.js
+++ ball_that_bounces/script.js
@@ -1,149 +1,152 @@
 const canvas = document.getElementById("game");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 const settings = {
-    quantityOfBalls: 10,
-    field: {
-        width: 400,
-        height: 400
-    },
-
-}
+  quantityOfBalls: 10,
+  field: {
+    width: 400,
+    height: 400,
+  },
+};
 
 let playing = true;
 
 let raf;
 
 function imperfection() {
-    return Math.random() * .1;
-    
+  return Math.random() * 0.1;
 }
 
 function setup() {
-    ctx.canvas.width = settings.field.width;
-    ctx.canvas.height = settings.field.height;
+  ctx.canvas.width = settings.field.width;
+  ctx.canvas.height = settings.field.height;
 }
 
 let balls = [];
 
 function arrayOfBalls() {
-    while (settings.quantityOfBalls > 0) {
-        balls.push(makeBall());
-        settings.quantityOfBalls -= 1;
-    }
+  while (settings.quantityOfBalls > 0) {
+    balls.push(makeBall());
+    settings.quantityOfBalls -= 1;
+  }
 }
 
 function makeBall() {
-    const ball = {
-        x: 0,
-        y: 1,
-        edges: {
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-        },
-        size: 15,
-        color: 'red',
-        velocity: {
-            x:1,
-            y:1,
-        },
-        imperfectBounce: false,
+  const ball = {
+    x: 0,
+    y: 1,
+    edges: {
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+    },
+    size: 15,
+    color: "red",
+    velocity: {
+      x: 1,
+      y: 1,
+    },
+    imperfectBounce: false,
+    physics: {
+      gravity: true,
+    },
+    calculateEdges: function () {
+      this.edges.top = this.y;
+      this.edges.bottom = this.y + this.size;
+      this.edges.left = this.x;
+      this.edges.right = this.x + this.size;
+    },
 
-        calculateEdges: function() {
-            this.edges.top = this.y;
-            this.edges.bottom = this.y + this.size;
-            this.edges.left = this.x;
-            this.edges.right = this.x + this.size;
-        },
+    move: function () {
+  
+      this.x += this.velocity.x;
+      this.y += this.velocity.y;
+      this.calculateEdges();
+      if (this.physics.gravity) {
+        this.gravity();
+      }
+      if (this.imperfectBounce) {
+        this.velocity.x += imperfection();
+        this.velocity.y += imperfection();
+        this.imperfectBounce = false;
+      }
+      if (this.collideVertical()) {
+        this.bounceVertical();
+      }
 
-        move: function() {
-          this.x += this.velocity.x;
-          this.y += this.velocity.y;
-          this.calculateEdges();
-        if (this.imperfectBounce) {
-            this.velocity.x += imperfection();
-            this.velocity.y += imperfection();
-            this.imperfectBounce = false;
-        }
-          if (this.collideVertical()) {
-              this.bounceVertical();
-          } 
-        
-          if 
-              (this.collideHorizontal()){
-              this.bounceHorizontal()
-          }
-        },
+      if (this.collideHorizontal()) {
+        this.bounceHorizontal();
+      }
+    },
 
-        collideVertical: function() {
-            if (ball.edges.left <= 0 ||
-                ball.edges.right >= settings.field.width ) {
-                    return true
-                }
-        },
+    collideVertical: function () {
+      if (ball.edges.left <= 0 || ball.edges.right >= settings.field.width) {
+        return true;
+      }
+    },
 
+    collideHorizontal: function () {
+      if (ball.edges.top <= 0 || ball.edges.bottom >= settings.field.height) {
+        return true;
+      }
+    },
 
-        collideHorizontal: function() {
-            if (ball.edges.top <= 0 ||
-                ball.edges.bottom >= settings.field.height ) {
-                    return true
-                }
-        },
+    bounceVertical: function () {
+      this.velocity.x = -this.velocity.x;
+      this.imperfectBounce = true;
+    },
 
-        bounceVertical: function() {
-            this.velocity.x = -this.velocity.x;
-            this.imperfectBounce = true;
-        },
+    bounceHorizontal: function () {
+      this.velocity.y = -this.velocity.y;
+      this.imperfectBounce = true;
+    },
 
-        bounceHorizontal: function() {
-            this.velocity.y = -this.velocity.y;
-            this.imperfectBounce = true;
-        },
+    gravity: function () {
+      this.velocity.y *= 0.99;
+      this.velocity.y += 0.25;
+    },
 
-        draw: function() {
-            this.move();
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.size, this.size);
-        }
-    }
+    draw: function () {
+      this.move();
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.size, this.size);
+    },
+  };
 
-    return ball;
+  return ball;
 }
 
 const field = {
-    draw: function() {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(0,0,settings.field.width, settings.field.height);
-    }
-}
+  draw: function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 0, settings.field.width, settings.field.height);
+  },
+};
 
 function render() {
-    field.draw();
-    balls.forEach(ball => {
-        ball.draw();
-    })
-    
+  field.draw();
+  balls.forEach((ball) => {
+    ball.draw();
+  });
 
-    raf = requestAnimationFrame(render);
+  raf = requestAnimationFrame(render);
 }
 
 function pause() {
-    cancelAnimationFrame(raf);
+  cancelAnimationFrame(raf);
 }
 
-
-canvas.addEventListener('click', function(){
-    if (playing){
-        pause();
-        playing = false;
-    } else {
-        render();
-        playing = true;
-    }
-})
+canvas.addEventListener("click", function () {
+  if (playing) {
+    pause();
+    playing = false;
+  } else {
+    render();
+    playing = true;
+  }
+});
 
 arrayOfBalls();
 
