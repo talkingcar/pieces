7ae84c624b1bc1cd85691f6b8995d84f616aa82d
Sam Brown <talkingcar@mac.com>
2020-08-01 09:08:46 -0400

    add keyboard input to move block

--- block_that_user_moves/script.js
+++ block_that_user_moves/script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+
 let raf;
+
+function setup() {
+  ctx.canvas.width = settings.field.width;
+  ctx.canvas.height = settings.field.height;
+}
 
 const settings = {
   field: {
@@ -10,9 +16,83 @@ const settings = {
     color: "grey"
   }
 }
+const blockOne = makeBlock(
+  settings.field.width * .5,
+  settings.field.height * .75,
+  'black');
 
-function makeBlock() {
+const blockTwo = makeBlock(
+  1,2
+)
 
+
+const currentInput = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+}
+
+document.addEventListener('keydown', keyDownHandler, false)
+document.addEventListener('keyup', keyUpHandler, false)
+
+function keyDownHandler(event) {
+  console.log(event)
+  if (
+    event.key == "ArrowUp" || 
+    event.key == "w") {
+      event.preventDefault();
+      currentInput.up = true;
+  }
+  
+  if (
+    event.key == "ArrowDown" ||
+    event.key == "s") {
+      event.preventDefault();
+      currentInput.down = true;
+  }
+  
+  if (
+    event.key == "ArrowLeft" ||
+    event.key == "a") {
+      event.preventDefault();
+      currentInput.left = true;
+  }
+  
+  if (
+    event.key == "ArrowRight" ||
+    event.key == "d") {
+      event.preventDefault();
+      currentInput.right = true;
+  }
+console.log(currentInput)
+}
+
+function keyUpHandler(event) {
+  if (
+    event.key == "ArrowUp" || 
+    event.key == "w") { 
+    currentInput.up = false;
+  }
+  
+  if (
+    event.key == "ArrowDown" ||
+    event.key == "s") {
+    currentInput.down = false;
+  }
+
+  if (
+    event.key == "ArrowLeft" ||
+    event.key == "a") {
+    currentInput.left = false;
+  }
+  
+  if (
+    event.key == "ArrowRight" ||
+    event.key == "d") {
+    currentInput.right = false;
+  }
+console.log(currentInput)
 }
 
 
@@ -24,12 +104,49 @@ const field = {
   },
 };
 
+function makeBlock(x,y,color = 'pink') {
+  const block = {
+    x: x,
+    y: y,
+    width: 50,
+    height: 10,
+    color: color,
+    speed: 2,
 
+    moveTo: function (x,y) {
+      this.x = x;
+      this.y = y;
+    },
+
+    inputMove: function () {
+      if (currentInput.up) {
+        this.y -= this.speed;
+      }
+      if (currentInput.down) {
+        this.y += this.speed;
+      }
+      if (currentInput.left) {
+        this.x -= this.speed;
+      }
+      if (currentInput.right)
+        this.x += this.speed;
+    },
+
+    draw: function () {
+      this.inputMove();
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  };
+  return block
+}
 
 function render() {
-  field.draw()
+  field.draw();
+  blockOne.draw();
+  blockTwo.draw();
   raf = requestAnimationFrame(render);
 }
 
-
+setup();
 raf = requestAnimationFrame(render);
